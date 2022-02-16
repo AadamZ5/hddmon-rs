a9e3db9977a2b108e17c8fb01f15769903f7fb67
Aadam Zocolo <azocolo@gmail.com>
2022-02-16 00:37:16 -0500

    Abstracted device creation code

--- src/device.rs
+++ src/device.rs
@@ -1,6 +1,6 @@
+use hdd::{ata::ATADevice, scsi::SCSIDevice, Device as HddDevice};
+use std::io::Result;
 use std::path::{Path, PathBuf};
-
-use hdd::{ata::ATADevice, device::Type, scsi::SCSIDevice};
 
 #[derive(Debug)]
 pub enum DeviceType {
@@ -23,4 +23,32 @@ impl std::fmt::Display for DeviceType {
 pub struct Device {
     pub path: PathBuf,
     pub device_type: DeviceType,
+}
+
+impl Device {
+    pub fn new(path: &Path) -> Result<Device> {
+        let dev = HddDevice::open(path)?;
+
+        let new_dev = Device {
+            path: path.to_path_buf(),
+            device_type: match dev.get_type() {
+                Err(_) => DeviceType::Unknown,
+                Ok(hdd::Type::SCSI) => {
+                    // Stolen from crate hdd source code
+                    // check whether devices replies to ATA PASS-THROUGH
+                    let satdev = ATADevice::new(SCSIDevice::new(dev));
+                    match hdd::ata::misc::Misc::get_device_id(&satdev) {
+                        // this is really an ATA device
+                        Ok(_) => DeviceType::ATA(satdev),
+                        // nnnnope, plain SCSI
+                        Err(hdd::ata::misc::Error::SCSI(hdd::scsi::ATAError::NotSupported)) => {
+                            DeviceType::SCSI(satdev.unwrap())
+                        }
+                        _ => DeviceType::SCSI(satdev.unwrap()),
+                    }
+                }
+            },
+        };
+        return Ok(new_dev);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod device;
 
 use std::io::Error;
 
-use hdd::{ata::ATADevice, list_devices, scsi::SCSIDevice, Device};
+use hdd::list_devices;
 
-use crate::device::{Device as HMDevice, DeviceType};
+use crate::device::Device as HMDevice;
 
 fn main() -> Result<(), Error> {
     println!("Hello, world!");
@@ -16,32 +16,9 @@ fn main() -> Result<(), Error> {
     //Get the devices, filter out ones that failed, and turn them into our special device struct
     let devices = devices
         .iter()
-        .map(|x| (Device::open(x), x))
-        .filter(|(dev, _)| !dev.is_err())
-        .map(|(dev, path)| (dev.unwrap(), path))
-        .map(|(dev, path)| {
-            let new_dev = HMDevice {
-                path: path.to_path_buf(),
-                device_type: match dev.get_type() {
-                    Err(_) => DeviceType::Unknown,
-                    Ok(hdd::Type::SCSI) => {
-                        // Stolen from crate hdd source code
-                        // check whether devices replies to ATA PASS-THROUGH
-                        let satdev = ATADevice::new(SCSIDevice::new(dev));
-                        match hdd::ata::misc::Misc::get_device_id(&satdev) {
-                            // this is really an ATA device
-                            Ok(_) => DeviceType::ATA(satdev),
-                            // nnnnope, plain SCSI
-                            Err(hdd::ata::misc::Error::SCSI(hdd::scsi::ATAError::NotSupported)) => {
-                                DeviceType::SCSI(satdev.unwrap())
-                            }
-                            _ => DeviceType::SCSI(satdev.unwrap()),
-                        }
-                    }
-                },
-            };
-            return new_dev;
-        })
+        .map(|x| HMDevice::new(x))
+        .filter(|dev| !dev.is_err())
+        .map(|dev| dev.unwrap())
         .collect::<Vec<HMDevice>>();
 
     for dev in devices {
